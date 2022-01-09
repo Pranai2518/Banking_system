625e005986aa151deb4d367da5433b56fc0c65ec
Pranai Raj <kaligithipranai@gmail.com>
2022-01-09 14:08:37 +0530

    navbar update

---
+++ nav.php
@@ -0,0 +1,23 @@
+<!-- ----navbar---- -->
+<nav class="navbar nav-expand-md nav-light bg-light">
+    <a class="nav-brand" href="index.php" style="color: #C0392B;"><b>ORIGIN BANK</b></b></a>
+    <button class="nav-toggle" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="nav-toggle-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="collapsiblenav">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color: #C0392B;"><b>Home</b></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="createuser.php" style="color: #C0392B"><b>Create User</b></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color: #C0392B"><b>Transfer Money</b></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color: #C0392B"><b>Transaction History</b></a>
+            </li>
+        </ul>
+    </div>
+</nav>
