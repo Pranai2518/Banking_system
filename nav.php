9c2611ddf1fa2f6d245e3f28abfc718a68b4c3c2
Pranai Raj <75829770+Pranai2518@users.noreply.github.com>
2022-02-15 01:02:00 +0530

    Updated nav

--- nav.php
+++ nav.php
@@ -1,23 +1,22 @@
-<!-- ----navbar---- -->
-<nav class="navbar nav-expand-md nav-light bg-light">
-    <a class="nav-brand" href="index.php" style="color: #C0392B;"><b>ORIGIN BANK</b></b></a>
-    <button class="nav-toggle" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-        <span class="nav-toggle-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="collapsiblenav">
-        <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="index.php" style="color: #C0392B;"><b>Home</b></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="createuser.php" style="color: #C0392B"><b>Create User</b></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="transfermoney.php" style="color: #C0392B"><b>Transfer Money</b></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="transactionhistory.php" style="color: #C0392B"><b>Transaction History</b></a>
-            </li>
-        </ul>
-    </div>
-</nav>
+<!-- navbar --> 
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php" style="color : #FF4500;"><b>ORIGIN BANK</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color : #FF4500"><b>Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php" style="color : #FF4500;"><b>Create User</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color : #FF4500"><b>Transfer Money</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : #FF4500"><b>Transaction History</b></a>
+              </li>
+          </div>
+       </nav>
